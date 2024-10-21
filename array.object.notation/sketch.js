a305d71de33026c2df30e89194610e54369ee77b
Evalinge <7005064@spsd.sk.ca>
2024-10-20 20:23:04 -0600

    Switched orientations, updated cave width.

--- array.object.notation/sketch.js
+++ array.object.notation/sketch.js
@@ -32,7 +32,7 @@ function generateGround(howWide){
   let time = 0;
   let deltaTime = 0.007;
   for (let x = 0; x<width; x+= howWide){
-    let someHeight = noise(time)*height;
+    let someHeight = noise(time)*height + biker.height/4;
     let someEllipse = spawnEllipse(x, someHeight, howWide);
     ground.push(someEllipse);
     time+= deltaTime;
@@ -51,17 +51,20 @@ function spawnEllipse(leftAt, theHeight, theWidth){
 
 function displayBiker(){
   imageMode(CENTER);
-  image(biker, bikeX+biker.width/4/2, ground[bikeX].y+ground[bikeX].h/2-biker.height/4/2, biker.width/4, biker.height/-4*-1);
+  image(biker, bikeX+biker.width/4/2, ground[bikeX].y+ground[bikeX].h/2, biker.width/4, biker.height/-4*-1);
   keyPressed();
+  if (bikeX - biker.width/4 <= 0){
+    bikeX = width - 10;
+   }
 }
 
 function keyPressed(){
   if (keyIsPressed){
     if (keyCode === LEFT_ARROW){
-      bikeX += 5;
+      bikeX -= 5;
     }
     if (keyCode === RIGHT_ARROW){
-      bikeX-=5;
+      bikeX +=5;
     }
   }
 }
