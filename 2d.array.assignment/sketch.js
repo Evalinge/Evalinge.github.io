db766f43eed93dbecc6419e1c499f61fe530b539
Evalinge <7005064@spsd.sk.ca>
2024-10-30 14:12:33 -0600

    Added and played with new grids
    Added a covering grid, can toggle it but not the cells individually. I hope too resolve this by being able to locate which cell we are at and what state it is in.

--- 2d.array.assignment/sketch.js
+++ 2d.array.assignment/sketch.js
@@ -2,18 +2,24 @@
 // October 28, 2024 
 // 
 
-const GRID_SIZE = 40; 
-let cellSize; 
-let grid; 
-let rows; 
-let cols; 
+const GRID_SIZE = 20; 
 const EMPTY_TILE = 0;
 const LASER = 1; 
 const DIAMOND = 2; 
+const MASK = 3; 
+
+
+let cellSize; 
+let grid; 
+let coveringGrid;
+let isGridCovered;
+let maskedCell = {
+  isMasked: true,
+  value: MASK,
+};
 
 function preload(){
-  theLaser = loadImage("laser.png");
-  
+  theLaser = loadImage("theLaser.png");
 }
 
 
@@ -26,22 +32,25 @@ function setup() {
     cellSize = width/GRID_SIZE;
   }
   grid = createGrid(GRID_SIZE, GRID_SIZE);
+  coveringGrid = createCoveringGrid(GRID_SIZE, GRID_SIZE);
+  isGridcovered = createStateGrid(GRID_SIZE, GRID_SIZE); 
 }
 
 function draw() {
   background(220);
   showGrid();
+  showCoveringGrid(); 
 }
 
-function createGrid(GRID_SIZE, GRID_SIZE){
+function createGrid(rows, cols){
   let newGrid = []; 
-  for (let y = 0; y < GRID_SIZE; y++){
+  for (let y = 0; y < cols; y++){
     newGrid.push([]);
-    for (let x = 0; x < GRID_SIZE; x++){
-      if (random(0, 100) <= 25){
+    for (let x = 0; x < rows; x++){
+      if (random(0, 100) <= 15){
         newGrid[y].push(LASER);
       }
-      else if (random(0, 100) > 25 && random(100) <= 40){
+      else if (random(0, 100) > 15 && random(100) <= 25){
         newGrid[y].push(DIAMOND);
       }
       else{
@@ -53,9 +62,10 @@ function createGrid(GRID_SIZE, GRID_SIZE){
 }
 
 function showGrid(){
-  for (let y = 0; y<GRID_SIZE; y++)
+  for (let y = 0; y<GRID_SIZE; y++) {
     for (let x = 0; x<GRID_SIZE; x++){
       if (grid[y][x] === LASER){
+        noFill();
         image(theLaser, x*cellSize, y*cellSize, cellSize, cellSize);
       }
       else if (grid[y][x] === EMPTY_TILE){
@@ -64,6 +74,51 @@ function showGrid(){
       else if(grid[y][x] === DIAMOND){
         fill("blue"); 
       }
+      
       rect(x*cellSize, y*cellSize, cellSize, cellSize);
     }
   }
+}
+
+function createCoveringGrid(rows, cols){
+  let newCoveringGrid = []; 
+  for (let y = 0; y < cols; y++){
+    newCoveringGrid.push([]);
+    for (let x = 0; x < rows; x++){
+      newCoveringGrid[y].push(maskedCell.value);
+    }
+  }
+  return newCoveringGrid;
+}
+
+function createStateGrid(rows, cols){
+  let newisGridCovered = [];
+  for (let y = 0; y <cols; y++){
+    newisGridCovered.push([]);
+    for (let x = 0; x < rows; x++){
+      newisGridCovered[y].push(maskedCell.isMasked);
+    }
+  }
+  return newisGridCovered;
+}
+
+
+function showCoveringGrid(){
+  for (let y = 0; y<GRID_SIZE; y++) {
+    for (let x = 0; x<GRID_SIZE; x++){
+      if (coveringGrid[y][x] === MASK && isGridCovered[y][x] === true){
+        fill("lightgray"); 
+      }
+      else if (coveringGrid[y][x] === MASK && cell.isMasked === false){
+        noFill(); 
+      }
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+function mousePressed(){
+  if (mouseButton === LEFT){
+    coveringGrid[floor(mouseX)][floor(mouseY)].cell.isMasked = !cell.isMasked; 
+  }
+}
